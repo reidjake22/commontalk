8254bdf0eb49456898ef2c87e5c00aa066258fea
jake <26926501+reidjake22@users.noreply.github.com>
2025-08-07 18:00:14 +0100

    up and running

--- backend/src/modules/points/save.py
+++ backend/src/modules/points/save.py
@@ -4,7 +4,7 @@ def save_points(conn, points: List[Dict]) -> None:
     """ Saves points to the database. """
     for point in points:
         try:
-            insert_point(conn, point['contribution_item_id'], point['point_value'], point['point_embedding'])
+            insert_point(conn, point[0], point[1], point[2])
         except Exception as e:
             print(f"Error inserting point for contribution item {point['contribution_item_id']}: {e}")
     conn.commit()
